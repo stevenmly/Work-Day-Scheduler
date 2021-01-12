6af9e12a493a4fb1daf2606bb562c8e038992cdc
Steven Ly <ly.steven16@gmail.com>
2021-01-11 22:16:26 -0600

    Added in current date at the top of application

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,15 @@
-var tasks 
+var createTask = function() {
+
+}
+
+var displayDate = function() {
+  var now = moment().format("dddd, MMMM Do YYYY");
+  var date = $(".jumbotron")
+  
+  var today = $("<p>").addClass("lead");
+  today.text(now);
+
+  date.append(today);
+}
+displayDate();
+
