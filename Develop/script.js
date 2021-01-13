6ef087e0e01d7cfb76c7f9f04ba2a48ca4ac4833
Steven Ly <ly.steven16@gmail.com>
2021-01-12 18:23:36 -0600

    Added in function to check timeblock status

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
-var createTask = function() {
+var checkTime = function() {
+  $( ".row" ).each(function( index ) {
+     var hourText = $(this).find(".hour").text();
+     var hour = moment(hourText, "HA");
+     var status = moment().diff(hour, "hours");
+     
+     if (status === 0) {
+       $(this).find(".description").addClass("present");
+     } else if (status > 0) {
+      $(this).find(".description").addClass("past");
+     } else if (status < 0) {
+      $(this).find(".description").addClass("future");
+     }
 
-}
+  });
+};
 
 var displayDate = function() {
   var now = moment().format("dddd, MMMM Do YYYY");
@@ -11,5 +24,21 @@ var displayDate = function() {
 
   date.append(today);
 }
+
+// task text was clicked
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
+
 displayDate();
+checkTime();
 
